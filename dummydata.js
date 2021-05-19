d1504bb79a86c72ed783ba03c484d25ff9994a53
unknown <codbs54@gmail.com>
2021-05-19 23:50:43 +0900

    커밋..

---
+++ dummydata.js
@@ -0,0 +1,39 @@
+// import profile from './img/chipmunk.jpg'
+const dummy = [
+  {
+    id:1,
+    userId:"chaeyoon Cha",
+    sns:"kakao",
+    profileImg:'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+    time:'2021.05.17 16:24',
+    text:'모바일버전 댓글목록을 구현하고 있습니다. 생각보다 어렵고 생각보다 재미있군요 아마 이런 이유때문에 무료로..어쩌고..',
+    comment:[],
+    like:5,
+    dislike:0
+  },
+  {
+    id:2,
+    userId:"chaeyoon Cha",
+    sns:"kakao",
+    profileImg:'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+    time:'2021.05.17 16:24',
+    text:'안녕하세욧',
+    comment:[],
+    like:0,
+    dislike:0
+  },
+  {
+    id:3,
+    userId:"chaeyoon Cha",
+    sns:"kakao",
+    profileImg:'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+    time:'2021.05.17 16:24',
+    text:'안녕하세욧',
+    comment:[],
+    like:0,
+    dislike:0
+  }
+]
+
+// export default dummy;
+export {dummy};
